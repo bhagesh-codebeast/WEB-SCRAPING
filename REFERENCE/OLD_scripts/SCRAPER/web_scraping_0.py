51bd99c6eb759b2c5317baa881e640d1cf8e5e77
bhagesh-codebeast <hunakunti.basavraj@learner.manipal.edu>
2020-07-27 18:29:41 +0530

    deleted old scripts

--- REFERENCE/OLD_scripts/SCRAPER/web_scraping_0.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://www.sigmaaldrich.com/chemistry/stockroom-reagents/learning-center/technical-library/solution-dilution-calculator.html'
-page = requests.get(url)
-
-soup = BeautifulSoup(page.content,'html.parser')
-print(soup.prettify())
